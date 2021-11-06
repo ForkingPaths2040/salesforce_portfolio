6e7d04c3372b0cfd591670ebd9dea0b0f7c0154f
ForkingPaths2040 <ambermoorexc@gmail.com>
2021-11-06 02:35:13 -0500

    Home page finished-ish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 function Hero(props) {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className='hero-container'>
+      <div className="flex fdrc jcc aic" style={{paddingTop:"10%"}}>
+        <div className='monty' style={{ fontWeight:'600', fontSize:'2.5vh'}}>The Agenda</div>
+        <div className='monty hero-title'><h1 style={{margin:'18px auto', textAlign:'center'}}>Be A Top Contributor</h1></div>
+        <div className='lato hero-text' style={{ textAlign: 'center', fontSize: '3.5vh' }}>I have a passion for solving problems ‘inside-the-box’ as there are limitations from the client, a project or an organization. My ability to adapt and serve my own expectations provides a seamless addition to any project or organization.</div>
+        <div className='flex fdrr call-to-action'>
+          <button className='act-button lato'>Projects</button>
+          <a style={{display:'contents'}}href="https://drive.google.com/file/d/1SdehA4vbdKgCjxebkMyIrfhH6Lf1yW5J/view?usp=sharing" target="blank"><button className='button-1 lato'>Resume</button></a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import '../App.css'
 
 function Navigation(props) {
   return (
-    <div>
-      <ul className='nav-items'>
+    <div className='nav-container'>
+      <ul className='nav-items flex jc-spbtw'>
             <Link to='/'><li>Home</li></Link>
             <Link to='/about'><li>About</li></Link>
             <Link to='/projects'><li>Projects</li></Link>
