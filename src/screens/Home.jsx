c5abfb87de57aedeacd0d9570df7f12ccaa37038
ForkingPaths2040 <ambermoorexc@gmail.com>
2021-11-06 02:48:22 -0500

    Photo credit Unsplash

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,6 +5,9 @@ function Home(props) {
   return (
     <div>
       <Hero />
+      <div style={{textAlign:'end', paddingRight:'10px', fontSize:'10px'}}>
+          <p>Photo provided by <a href='https://www.instagram.com/georginatheweiner/'><b>Georgie Cobbs</b></a> via <a href='https://unsplash.com/@georgie_cobbs'><b>Unsplash</b></a></p>
+      </div>
     </div>
   );
 }
